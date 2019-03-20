ae9d44cc07c35282fc563347d3d89364e4793c60
Steven Deferme <steven.deferme@gmail.com>
2019-03-20 16:32:25 +0100

    changes ewt model + server.js

--- server/models/ewt.js
+++ server/models/ewt.js
@@ -47,9 +47,33 @@ EwtSchema.methods.generateAuthToken = function() {
     var ewt = this;
     var access = 'auth';
     var token = jwt.sign({_id: ewt._id.toHexString(), access}, process.env.JWT_SECRET).toString();
-    ewt.token = ewt.tokens.concat([{access, token}]);
+    ewt.tokens = ewt.tokens.concat([{access, token}]);
     return ewt.save().then(() => {
         return token;
+    });
+};
+
+EwtSchema.methods.removeToken = function (token) {
+    var ewt = this;
+    return ewt.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
+EwtSchema.statics.findByToken = function (token) {
+    var Ewt = this;
+    var decoded;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (e) {
+        return Promise.reject();
+    }
+    return Ewt.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'token.access': 'auth'
     });
 };
 
